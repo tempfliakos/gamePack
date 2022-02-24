0e5eea1a214465439abf252e37b09b1da3729873
Koko <kornel.klimaj@telcotrend.hu>
2022-02-24 17:18:08 +0100

    design + smooth motion

--- scripts/fighter.js
+++ scripts/fighter.js
@@ -1,61 +1,59 @@
 "use strict";
 
 const canvas = document.getElementById('gameCanvas');
+const context = canvas.getContext('2d');
 
 function initCanvasSize() {
-    canvas.width = document.documentElement.clientWidth;
-    canvas.height = document.documentElement.clientHeight;
+	canvas.width = document.documentElement.clientWidth;
+	canvas.height = document.documentElement.clientHeight;
+	$('body').height(document.documentElement.clientHeight);
+}
 
-    $('body').height(document.documentElement.clientHeight);
+var keysDown = {};
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+var heroImage = new Image();
+heroImage.src = "../resources/hero.svg";
+class Hero {
+	constructor(name, positionX, positionY) {
+		context.clearRect(0, 0, innerWidth, innerHeight);
+		this.name = name;
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.speed = 300;
+		context.beginPath();
+		context.drawImage(heroImage, positionX, positionY, 40, 40);
+	}
+}
+
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
+	step(delta / 1000);
+	hero = new Hero(hero.name, hero.positionX, hero.positionY, false);
+	then = now;
+	requestAnimationFrame(main);
+}
+
+var step = function (modifier) {
+	if (87 in keysDown) {
+		hero.positionY -= hero.speed * modifier;
+	} else if (65 in keysDown) {
+		hero.positionX -= hero.speed * modifier;
+	} else if (83 in keysDown) {
+		hero.positionY += hero.speed * modifier;
+	} else if (68 in keysDown) {
+		hero.positionX += hero.speed * modifier;
+	}
 }
 
 initCanvasSize();
 window.addEventListener('resize', initCanvasSize);
-
-
-const context = canvas.getContext('2d');
-
-
-class Hero {
-    constructor(name, color, positionX, positionY, isEmpty) {
-        context.clearRect(0, 0, innerWidth, innerHeight);
-        this.name = name;
-        this.color = color;
-        this.positionX = positionX;
-        this.positionY = positionY;
-        context.beginPath();
-        context.arc(positionX, positionY, 30, 0, Math.PI * 2, false);
-        if (isEmpty) {
-            context.strokeStyle = color;
-            context.stroke();
-        } else {
-            context.fillStyle = color;
-            context.fill();
-        }
-    }
-}
-
-let hero = new Hero('Unkindled', 'green', 50, 50, false);
-let hero2 = new Hero('Cursed One', 'red', 100, 100, true);
-
-function animate() {
-    requestAnimationFrame(animate);
-    hero = new Hero(hero.name, hero.color, hero.positionX, hero.positionY, false);
-    //hero2 = new Hero(hero2.name, hero2.color, hero2.positionX, hero2.positionY, true);
-}
-
-animate();
-
-document.addEventListener('keydown', step);
-
-function step(e) {
-    if (e.code === 'KeyW') {
-        hero.positionY -= 5;
-    } else if (e.code === 'KeyA') {
-        hero.positionX -= 5;
-    } else if (e.code === 'KeyS') {
-        hero.positionY += 5;
-    } else if (e.code === 'KeyD') {
-        hero.positionX += 5;
-    }
-}
+let hero = new Hero('Unkindled', (canvas.width / 2) - 20, (canvas.height / 2) - 20);
+var then = Date.now();
+main();
